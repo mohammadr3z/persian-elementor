efdf30ad17629d298517ef01b5997fa5475cdc53
mohammadr3z <sms_co1993@yahoo.com>
2019-03-28 14:44:54 +0430

    Persian Elementor

--- includes/editor.php
+++ includes/editor.php
@@ -3,7 +3,3 @@
 add_action('elementor/editor/before_enqueue_scripts', function() {
 wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/admin-font.css' );
 });
-
-
-__( 'Persian Elementor', 'persian-elementor' );
-
--- persian-elementor.php
+++ persian-elementor.php
@@ -1,11 +1,11 @@
-﻿<?php
+<?php
 /**
- * Plugin Name: Persian Language For Elementor
- * Plugin URI: https://themeboutique.ir/contact
- * Description: افزونه فارسی ساز صفحه ساز المنتور
+ * Plugin Name: المنتور فارسی
+ * Plugin URI: http://elementorfa.ir/contact
+ * Description: فارسی ساز نسخه پرو المنتور
  * Version: 1.0.0
- * Author: Persian Elementor
- * Author URI: https://themeboutique.ir
+ * Author: المنتور فارسی
+ * Author URI: https://elementorfa.ir
  * Text Domain: persian-elementor
  * License: GPL2
  */
@@ -23,7 +23,7 @@ require_once(PERSIAN_ELEMENTOR.'includes/editor.php');
 
 add_action( 'init', 'myplugin_load_textdomain' );
 function myplugin_load_textdomain() {
-load_plugin_textdomain( 'elementor-pro', false, basename( dirname( __FILE__ ) ) . '/languages/elementor-pro' ); 
+load_plugin_textdomain( 'elementor-pro', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 load_plugin_textdomain( 'persian-elementor', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
 
