2d38e9b2f55bb3ddeef4c00ada87f791cae99f45
mohammadr3z <sms_co1993@yahoo.com>
2019-03-28 14:20:08 +0430

    Revert "S"
    This reverts commit 5a51fff0e2e069a74b19476a6e73a632a13583c0.

--- includes/editor.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-add_action('elementor/editor/before_enqueue_scripts', function() {
-wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/admin-font.css' );
-});
-
-
-__( 'Persian Elementor', 'persian-elementor' );
-
