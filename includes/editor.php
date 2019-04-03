cdf1f94632678d32663e5955dd18119269cf2500
mohammadr3z <sms_co1993@yahoo.com>
2019-04-03 20:05:05 +0430

    Update editor.php

--- includes/editor.php
+++ includes/editor.php
@@ -1,9 +1,11 @@
 <?php
-
+//Add font to editor
 add_action('elementor/editor/before_enqueue_scripts', function() {
 wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/admin-font.css' );
 });
 
+
+//Add font to frontend
 add_action('elementor/frontend/after_enqueue_styles', function() {
 wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/front-font.css' );
 });
