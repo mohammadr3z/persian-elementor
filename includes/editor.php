c2ce5671faf14d78135a875e09b597b8146d7a34
mohammadr3z <sms_co1993@yahoo.com>
2019-04-03 19:40:25 +0430

    Persian Elemenetor

--- includes/editor.php
+++ includes/editor.php
@@ -4,6 +4,11 @@ add_action('elementor/editor/before_enqueue_scripts', function() {
 wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/admin-font.css' );
 });
 
+add_action('elementor/frontend/after_enqueue_styles', function() {
+wp_enqueue_style( 'persian-elementor','/wp-content/plugins/persian-elementor/includes/css/front-font.css' );
+});
+
+
 
 __( 'Persian Elementor', 'persian-elementor' );
 
--- persian-elementor.php
+++ persian-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: المنتور فارسی
  * Plugin URI: http://elementorfa.ir/contact
  * Description: افزونه فارسی ساز صفحه ساز المنتور و بهبود ویرایشگر زنده
- * Version: 1.2.0
+ * Version: 1.2
  * Author: المنتور فارسی
  * Author URI: https://elementorfa.ir
  * Text Domain: persian-elementor
