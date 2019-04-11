d8840c82c9984bf2107e4de91710a5db99af0307
mohammadr3z <sms_co1993@yahoo.com>
2019-04-11 13:34:23 +0430

    Update persian-elementor.php

--- persian-elementor.php
+++ persian-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: المنتور فارسی
  * Plugin URI: http://elementorfa.ir
  * Description: افزونه فارسی ساز صفحه ساز المنتور به همراه اضافه شدن فونت های فارسی در بخش تایپوگرافی و بهبود ظاهر ویرایشگر برای زبان فارسی
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: المنتور فارسی
  * Author URI: https://elementorfa.ir
  * Text Domain: persian-elementor
