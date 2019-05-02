a28d0fe29718aee61c5eb8c95dc1c7961fd7dfe8
mohammadr3z <sms_co1993@yahoo.com>
2019-05-03 00:07:48 +0430

    Add plugin to wordpress repository

--- includes/dashboard.php
+++ includes/dashboard.php
@@ -18,18 +18,18 @@
 function dashboard_custom_feed_output() {
 	
 		
-		echo '<a target=_blank\" href="https://elementorfa.ir/"><img src="https://elementorfa.ir/wp-content/uploads/2019/04/PersianElementor.png" style=height:100px; /></a>';
-		echo '<p>پشتیبانی فارسی افزونه صفحه ساز المنتور</p>';
-		echo '<li><a target=_blank\" href="http://bit.ly/wp-dashboad-buy">خرید افزونه المنتور فارسی</a></li>';
+		echo '<a target=_blank\" href="https://elementorfa.ir/"><img src="https://elementorfa.ir/wp-content/uploads/2019/04/PersianElementor2.png" style=height:100px; /></a>';
+		echo '<p style="font-weight: 700; border-bottom: 1px solid #eee; margin: 0 -12px; padding: 6px 12px;	">پشتیبانی فارسی افزونه صفحه ساز المنتور</p>';
+		echo '<li style="margin-top: 10px;"><a target=_blank\" href="http://bit.ly/wp-dashboad-buy">خرید افزونه المنتور فارسی</a></li>';
 		echo '<li><a target=_blank\" href="http://bit.ly/elementorfa-shop">فروشگاه محصولات المنتور</a></li>';
 		echo '</br>';
-		echo '<p>آخرین محصولات المنتور فارسی :</p>';
+		echo '<p style ="font-weight: 700; border-bottom: 1px solid #eee; margin: 0 -12px; padding: 6px 12px;">آخرین مقالات المنتور فارسی :</p>';
 
-		echo '<div class="rss-widget">';
+		echo '<div style="margin-top: 10px;" class="rss-widget">';
      wp_widget_rss_output(array(
-          'url' => 'https://rss.app/feeds/csyDgxgCjckCT2AJ.xml',
+          'url' => 'https://elementorfa.ir/feed',
           'title' => 'المنتور فارسی',
-          'items' => 4,
+          'items' => 3,
           'show_summary' => 1,
           'show_author' => 0,
           'show_date' => 1 
