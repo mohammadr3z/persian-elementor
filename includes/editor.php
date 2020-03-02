a90f61bcdcdff9bb36fe41ad63e417d243dc3789
mohammadr3z <sms_co1939@yahoo.com>
2020-03-03 02:01:22 +0330

    Change import css to wp_enqueue_scripts

--- includes/editor.php
+++ includes/editor.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * ElementorFa fonts.
+ * ElementorFa font.
  *
  *
- * @since 1.0.0
+ * @since 1.9.1
  */
  
 // enqueue css file into editor
@@ -12,7 +12,13 @@
 });
 
 
-//enqueue css file for front-end
-	add_action('elementor/frontend/after_enqueue_styles', function() {
-		wp_enqueue_style( 'persian-elementor',plugins_url( 'assets/frontend-persian.min.css', __FILE__ ) );
-});
+// Register style sheet.
+add_action( 'wp_enqueue_scripts', 'register_persian_elementor_font' );
+
+/**
+ * Enqueue style sheet.
+ */
+function register_persian_elementor_font() {
+    wp_register_style( 'persian-elementor-font', 'https://rawcdn.githack.com/mohammadr3z/CDN_Font/aa30dc93b0f2f94ca29521d33a9d32dac88795af/font.css' );
+    wp_enqueue_style( 'persian-elementor-font' );
+}
