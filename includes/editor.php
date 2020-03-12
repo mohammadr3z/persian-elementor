de91e8c8efdb82066f1226de34e6a17965b0894c
mohammadr3z <sms_co1939@yahoo.com>
2020-03-12 18:10:13 +0330

    change font res

--- includes/editor.php
+++ includes/editor.php
@@ -3,22 +3,25 @@
  * ElementorFa font.
  *
  *
- * @since 1.9.1
+ * @since 1.9.3
  */
  
-// enqueue css file into editor
+// Enqueue Editor
 	add_action('elementor/editor/before_enqueue_scripts', function() {
 		wp_enqueue_style( 'persian-elementor',plugins_url( 'assets/editor-rtl.min.css', __FILE__ ) );
 });
 
+// Enqueue Preview
+	add_action('elementor/preview/enqueue_styles', function() {
+		wp_enqueue_style( 'persian-elementor',plugins_url( 'assets/preview-rtl.css', __FILE__ ) );
+});
 
-// Register style sheet.
+
+// Register StyleSheet
 add_action( 'wp_enqueue_scripts', 'register_persian_elementor_font' );
 
-/**
- * Enqueue style sheet.
- */
+// Enqueue Front
 function register_persian_elementor_font() {
-    wp_register_style( 'persian-elementor-font', 'https://rawcdn.githack.com/mohammadr3z/CDN_Font/aa30dc93b0f2f94ca29521d33a9d32dac88795af/font.css' );
+    wp_register_style( 'persian-elementor-font', 'https://rawcdn.githack.com/mohammadr3z/CDN_Font/6986c5023c5b098f734b2cbf97713e77f746507c/font.css' );
     wp_enqueue_style( 'persian-elementor-font' );
 }
