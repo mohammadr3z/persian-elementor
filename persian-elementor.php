41a818af9c327421ad317d6c5439ff77d2d8d816
mohammadr3z <sms_co1939@yahoo.com>
2021-02-15 23:57:32 +0330

    Update to 2.3.7

--- persian-elementor.php
+++ persian-elementor.php
@@ -3,7 +3,7 @@
  * Plugin Name: المنتور فارسی
  * Plugin URI: 
  * Description: بسته فارسی ساز افزونه المنتور پرو به همراه اضافه شدن 12 فونت فارسی، تقویم شمسی برای المنتور، قالب های آماده فارسی در کتابخانه المنتور و آیکون های ایرانی
- * Version: 2.3.6
+ * Version: 2.3.7
  * Author: المنتور فارسی
  * Author URI: 
  * Text Domain: persian-elementor
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PERSIAN_ELEMENTOR_VERSION', '2.3.6' );
+define( 'PERSIAN_ELEMENTOR_VERSION', '2.3.7' );
 
 
 define( 'PERSIAN_ELEMENTOR', plugin_dir_path(__FILE__));
