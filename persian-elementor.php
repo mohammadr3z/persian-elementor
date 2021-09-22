614144b565c34d6322ff80159f17aa41457cad5f
mohammadr3z <sms_co1993@yahoo.com>
2021-09-23 01:13:00 +0330

    Update persian-elementor.php

--- persian-elementor.php
+++ persian-elementor.php
@@ -3,13 +3,13 @@
  * Plugin Name: المنتور فارسی
  * Plugin URI: 
  * Description: بسته فارسی ساز افزونه المنتور پرو به همراه اضافه شدن 12 فونت فارسی، تقویم شمسی برای المنتور، قالب های آماده فارسی در کتابخانه المنتور و آیکون های ایرانی
- * Version: 2.4.8
+ * Version: 2.4.9
  * Author: المنتور فارسی
  * Author URI: 
  * Text Domain: persian-elementor
  * License: GPL2
- * Elementor tested up to: 3.4.3
- * Elementor Pro tested up to: 3.3.4
+ * Elementor tested up to: 3.4.4
+ * Elementor Pro tested up to: 3.4.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -31,7 +31,7 @@ add_action( 'plugins_loaded', 'persian_elementor_init' );
 function persian_elementor_init() {
 
     define( 'PERSIAN_ELEMENTOR', plugin_dir_path(__FILE__));
-    define( 'PERSIAN_ELEMENTOR_VERSION', '2.4.8' );
+    define( 'PERSIAN_ELEMENTOR_VERSION', '2.4.9' );
 }
  
 final class Persian_Elementor {
@@ -42,7 +42,7 @@ final class Persian_Elementor {
 	 * @since 2.3.10
 	 * @var string The plugin version.
 	 */
-	const VERSION = '2.4.8';
+	const VERSION = '2.4.9';
 
 	/**
 	 * Minimum Elementor Version
